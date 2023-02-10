87e1d7e2be3c5563cf89313d0168114f896e785c
IvyWanjiru <ivykwanjiru2003@gmail.com>
2023-02-10 10:52:50 +0300

    python list

---
+++ lists/list.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    nums.append(i)
+
+for i  in range(len(nums)):
+    print(num[i])
---
+++ lists/listlist.py
@@ -0,0 +1,29 @@
+#python list
+names = ["Ivy","Grace","Kui","Keith","Sam","Kith"]
+numbers = [3,4,5,6,7465,56,62,58,65,6,47,59,34,33,49,32,42]
+decimals = [45.7,3435.4,32545.43,434.32]
+
+#print(names[1])
+
+#print(len(numbers)) #length of my list
+
+#print(sum(numbers))
+
+#
+
+
+#print(numbers.index(6))
+
+#print(names.index("Ivy"))
+
+#Append adds an element to the end of a list
+
+for i in range(len(numbers)):
+    print(numbers[i])
+
+    print("\n")
+    numbers.append(500)
+
+    #after appending
+for i in range(len(numbers)):
+    print(numbers[i])
